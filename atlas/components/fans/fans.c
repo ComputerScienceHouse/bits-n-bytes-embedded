671f8a27bde4eb475678fa799cfe2533111221ae
IsaacIngram <me@isaacingram.dev>
2025-10-13 18:39:09 -0400

    Comment out tach stuff

--- atlas/components/fans/fans.c
+++ atlas/components/fans/fans.c
@@ -62,18 +62,18 @@ void fans_init() {
     ledc_update_duty(FAN_SPEED_MODE, FAN_LEDC_CHANNEL);
 
 
-    gpio_config_t tac_conf = {
-            .intr_type = GPIO_INTR_NEGEDGE,
-            .mode = GPIO_MODE_INPUT,
-            .pin_bit_mask = (1ULL << INTAKE_FAN_TAC_PIN) | (1ULL << EXHAUST_FAN_TAC_PIN),
-            .pull_up_en = GPIO_PULLUP_ENABLE,
-            .pull_down_en = GPIO_PULLDOWN_DISABLE
-    };
-    gpio_config(&tac_conf);
+//    gpio_config_t tac_conf = {
+//            .intr_type = GPIO_INTR_NEGEDGE,
+//            .mode = GPIO_MODE_INPUT,
+//            .pin_bit_mask = (1ULL << INTAKE_FAN_TAC_PIN) | (1ULL << EXHAUST_FAN_TAC_PIN),
+//            .pull_up_en = GPIO_PULLUP_ENABLE,
+//            .pull_down_en = GPIO_PULLDOWN_DISABLE
+//    };
+//    gpio_config(&tac_conf);
 
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(INTAKE_FAN_TAC_PIN, tach_isr, NULL);
-    gpio_isr_handler_add(EXHAUST_FAN_TAC_PIN, tach_isr, NULL);
+//    gpio_install_isr_service(0);
+//    gpio_isr_handler_add(INTAKE_FAN_TAC_PIN, tach_isr, NULL);
+//    gpio_isr_handler_add(EXHAUST_FAN_TAC_PIN, tach_isr, NULL);
 
 }
 
